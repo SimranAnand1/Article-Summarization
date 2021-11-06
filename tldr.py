2376b3a60286d9c5427bc8fa50c771cced2449d6
Simran Anand <67438489+SimranAnand1@users.noreply.github.com>
2021-11-06 20:27:05 +0530

    Create tldr.py

---
+++ tldr.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from transformers import pipeline
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+st.write("# TLDR-izer")
+article = st.text_area('Text to be summarized:', height=200)
+
+if st.button("Do the magic"):
+    summarizer = pipeline("summarization")
+    output = summarizer(article, min_length=50, max_length=90)
+    st.write(output[0]['summary_text'])
